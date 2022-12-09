168d135303f631ee14474091f66cc457189b04af
antonioseet <antonioseet1@gmail.com>
2022-12-09 13:29:38 -0800

    UI works completely.

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,14 +1,13 @@
 # pip install "pymongo[srv]"
-
 from pymongo import MongoClient
+
+gmaxView = 18291006
+gmaxLen = 4597587
 
 def get_database():
     CONNECTION_STRING = "mongodb+srv://bag:Backpack!@teambag.ywpuiau.mongodb.net/test"
     client = MongoClient(CONNECTION_STRING)
     return client
-  
-def printResult(result):
-    print(str(result['uploader']) + ' | ' + str(result['category']) + ' | ' + str(result['length']) + ' | ' + str(result['views']))
 
 def getCategoriesList():
     db = get_database()
@@ -25,33 +24,105 @@ def getTopKResults(k: int, option: int) -> list:
     results = list()
 # top categories
     if option == 0:
-        results = db['VideoData']['videos'].aggregate([{'$group': {'_id': '$category', 'count': {'$sum': 1}}}, {'$sort': {'count': -1}}, {'$limit': 5}])
+        results = db['VideoData']['videos'].aggregate([{'$group': {'_id': '$category', 'count': {'$sum': 1}}}, {'$sort': {'count': -1}}, {'$limit': int(k)}])
         return topCategoriesStrings(results)
 # most popular - by views
     elif option == 1:
-        results = db['VideoData']['videos'].aggregate([{'$sort' : {'views': -1}}, {'$limit': k}])
-        return mostPopularStrings()
+        results = db['VideoData']['videos'].aggregate([{'$sort' : {'views': -1}}, {'$limit': int(k)}])
+        return mostPopularStrings(results)
 # top rated
     else:
-        results = db['VideoData']['videos'].aggregate([{'$sort' : {'rate': -1, 'ratings': -1}}, {'$limit': k}])
-        return topRatedStrings()
-    
+        results = db['VideoData']['videos'].aggregate([{'$sort' : {'rate': -1, 'ratings': -1}}, {'$limit': int(k)}])
+        #return topRatedStrings(results)
+
     return results
+    
+def getVideoResults(minView: int, maxView: int, minLen: int, maxLen: int, minrate: int, maxrate: int, category: str):
+    db = get_database()
+    results = list()
+
+    if maxView == 0:
+        maxView = gmaxView
+
+    if maxLen == 0:
+        maxLen == gmaxLen
+
+    #results = db['VideoData']['videos'].aggregate([{'$bucket': {'groupBy': '$rate', 'boundaries': [ 0, 1, 2, 3, 4, 5.1], 'output': {'counts': {'$sum': 1}}}}])
+    filter={
+    'views': {
+        '$gt': minView, 
+        '$lt': maxView
+    }, 
+    'rate': {
+        '$gt': minrate, 
+        '$lt': maxrate
+    }, 
+    'length': {
+        '$gt': minLen, 
+        '$lt': maxLen
+    }, 
+    'category': category}
+
+    results = db['VideoData']['videos'].find(filter=filter)
+    listRes = list(results)
+    print("Method results: " + str(list))
+
+    return videoResultsStrings(listRes)
+
+def getFrequencyResults(minView: int, maxView: int, minLen: int, maxLen: int, minrate: int, maxrate: int, category: str):
+    db = get_database()
+    results = list()
+
+    if maxView == 0:
+        maxView = gmaxView
+
+    if maxLen == 0:
+        maxLen == gmaxLen
+
+    #results = db['VideoData']['videos'].aggregate([{'$bucket': {'groupBy': '$rate', 'boundaries': [ 0, 1, 2, 3, 4, 5.1], 'output': {'counts': {'$sum': 1}}}}])
+    filter={
+    'views': {
+        '$gt': minView, 
+        '$lt': maxView
+    }, 
+    'rate': {
+        '$gt': minrate, 
+        '$lt': maxrate
+    }, 
+    'length': {
+        '$gt': minLen, 
+        '$lt': maxLen
+    }, 
+    'category': category}
+
+    results = db['VideoData']['videos'].find(filter=filter)
+    count = len(list(results))
+    return count
     
 def topCategoriesStrings(messyList: list):
     results = list()
     for item in messyList:
-        results.append(item['_id'])
+        results.append(str(item['_id']) + " | use count: " + str(item['count']))
     return results
 
 def mostPopularStrings(messyList: list):
     results = list()
     for item in messyList:
-        results.append(item['_id'])
+        formatted = "{:,}".format(item['views'])
+        results.append(str(item['uploader']) + " uploaded " + str(item['vidid'])+ " and received "  + str(formatted) + " views.")
     return results
 
 def topRatedStrings(messyList: list):
-    pass
+    results = list()
+    for item in messyList:
+        results.append(str(item['vidid']) + " uploaded by " + str(item['uploader'])+ ", received "  + str(item['ratings']) + " ratings and scored: " + str(item['rate']))
+    return results
+
+def videoResultsStrings(messyList: list):
+    results = list()
+    for item in messyList:
+        results.append(str(item['vidid']) + " uploaded by " + str(item['uploader'])+ ", received "  + str(item['views']) + " views and scored: " + str(item['rate']))
+    return results
 
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
@@ -59,12 +130,10 @@ if __name__ == "__main__":
     # Get the database
     db = get_database()
 
-    filter={'length': {'$gt': 0, '$lt': 1008}, 'category': 'Sports'}
-
-    #result contains a Cursor object
-    results = getTopKResults(5, 1)#db['VideoData']['videos'].find(filter=filter)
-
-    for result in results:
-        print(result)
+    #results = getTopKResults(10, 2)#db['VideoData']['videos'].find(filter=filter)
+    result = getVideoResults(200000, 1000000, 0, 1000, 4, 5, 'Sports')
+    print(result)
+    #for i in result:
+    #    print(i)
 
     print('__________')
--- UserInterface.py
+++ UserInterface.py
@@ -6,8 +6,65 @@ import DatabaseConnection as dbc
 
 def topKResults():
     count = 1
+    topKResultsBox.delete(0,END)
     for item in dbc.getTopKResults(kEntryBox.get(),comboBox.current()):
         topKResultsBox.insert(count, item)
+        count = count + 1
+
+def frequencyResult():
+    freqResultsBox.delete(0,END)
+
+    #separate the view count
+    viewCounts = viewEntryBox.get()
+    viewCountsList = viewCounts.split()
+
+    print(viewCountsList)
+    minViewCount = int(viewCountsList[0])
+    maxViewCount = int(viewCountsList[1])
+
+    #separate the length
+    lengthCounts = lengthEntryBox.get()
+    lengthCountsList = lengthCounts.split()
+
+    print(lengthCountsList)
+    minLength = int(lengthCountsList[0])
+    maxLength = int(lengthCountsList[1])
+    minRate = int(lengthCountsList[2])
+    maxRate = int(lengthCountsList[3])
+
+    categoryStr = comboBox2.get()
+
+    count = dbc.getFrequencyResults(minViewCount, maxViewCount, minLength, maxLength, minRate, maxRate, categoryStr)
+    freqResultsBox.insert(1, count)
+
+def videoResults():
+    rb.delete(0,END)
+
+    #separate the view count
+    viewCounts = ve.get()
+    viewCountsList = viewCounts.split()
+
+    print(viewCountsList)
+    minViewCount = int(viewCountsList[0])
+    maxViewCount = int(viewCountsList[1])
+
+    #separate the length
+    lengthCounts = eb.get()
+    lengthCountsList = lengthCounts.split()
+
+    print(lengthCountsList)
+    minLength = int(lengthCountsList[0])
+    maxLength = int(lengthCountsList[1])
+    minRate = int(lengthCountsList[2])
+    maxRate = int(lengthCountsList[3])
+
+    categoryStr = cb.get()
+
+    res = dbc.getVideoResults(minViewCount, maxViewCount, minLength, maxLength, minRate, maxRate, categoryStr)
+
+    count = 1
+    for item in res:
+        rb.insert(count, item)
         count = count + 1
 
 width = 1000
@@ -26,31 +83,88 @@ frame1 = Frame(master=notebook, width=width, height=height, bg="gray")
 frame1.pack(fill="both", expand=1)
 
 topKLabel = Label(frame1, text="Enter Top K integer below:", bg="gray")
-topKLabel.grid(row=0, column=0, padx=5, pady=5)
+topKLabel.place(relx=0.01, rely=0.02, anchor=tk.NW)
 
 kEntryBox = Entry(frame1)
-kEntryBox.grid(row=1, column=0, padx=5, pady=5)
+kEntryBox.place(relx=0.01, rely=0.05, anchor=tk.NW)
 
 optionLabl = Label(frame1, text="Choose an option:", bg="gray")
-optionLabl.grid(row=2, column=0, padx=5, pady=5)
+optionLabl.place(relx=0.01, rely=0.08, anchor=tk.NW)
 
 items = ['Top Categories', 'Most popular (Views)', 'Top Rated']
 comboBox = ttk.Combobox(frame1, values=items)
-comboBox.grid(row=3, column=0, padx=5, pady=5)
+comboBox.place(relx=0.01, rely=0.11, anchor=tk.NW)
 
 searchButton = Button(frame1, text="Search", command=topKResults)
-searchButton.grid(row=4, column=0, padx=5, pady=5)
+searchButton.place(relx=0.01, rely=0.14, anchor=tk.NW)
 
-topKResultsBox = Listbox(frame1)
-
-topKResultsBox.grid(padx=50, pady=30)
+topKResultsBox = Listbox(frame1, width=100, height=25)
+topKResultsBox.place(relx=0.01, rely=0.17, anchor=tk.NW)
 
 notebook.add(frame1, text="Top K Queries")
 
-####################################################################################################
+############################### FRAME 2 #############################################
 
 frame2 = Frame(notebook, width=width, height=height, bg="gray")
 frame2.pack(fill="both", expand=1)
-notebook.add(frame2, text="Second Tab")
+
+
+label1 = Label(frame2, text="Enter numbers in the form x y: x = min View count; y = max view count", bg="gray")
+label1.place(relx=0.01, rely=0.02, anchor=tk.NW)
+
+viewEntryBox = Entry(frame2)
+viewEntryBox.place(relx=0.01, rely=0.05, anchor=tk.NW)
+
+label1 = Label(frame2, text="Enter numbers in the form w x y z: w = minLength, x = maxLength, y = min rating, z = max rating", bg="gray")
+label1.place(relx=0.01, rely=0.08, anchor=tk.NW)
+
+lengthEntryBox = Entry(frame2)
+lengthEntryBox.place(relx=0.01, rely=0.11, anchor=tk.NW)
+
+viewLabel = Label(frame2, text="Choose a category to search:", bg="gray")
+viewLabel.place(relx=0.01, rely=0.14, anchor=tk.NW)
+
+items = dbc.getCategoriesList()
+comboBox2 = ttk.Combobox(frame2, values=items)
+comboBox2.place(relx=0.01, rely=0.17, anchor=tk.NW)
+
+searchButton2 = Button(frame2, text="Search", command=frequencyResult)
+searchButton2.place(relx=0.01, rely=0.2, anchor=tk.NW)
+
+freqResultsBox = Listbox(frame2, width=20, height=1, bg='gray')
+freqResultsBox.place(relx=0.01, rely=0.23, anchor=tk.NW)
+notebook.add(frame2, text="Categorization")
+
+##################################### FRAME 3 ##########################################
+
+frame3 = Frame(notebook, width=width, height=height, bg="gray")
+frame3.pack(fill="both", expand=1)
+
+
+label2 = Label(frame3, text="Enter numbers in the form x y: x = min View count; y = max view count", bg="gray")
+label2.place(relx=0.01, rely=0.02, anchor=tk.NW)
+
+ve = Entry(frame3)
+ve.place(relx=0.01, rely=0.05, anchor=tk.NW)
+
+l = Label(frame3, text="Enter numbers in the form w x y z: w = minLength, x = maxLength, y = min rating, z = max rating", bg="gray")
+l.place(relx=0.01, rely=0.08, anchor=tk.NW)
+
+eb = Entry(frame3)
+eb.place(relx=0.01, rely=0.11, anchor=tk.NW)
+
+cl = Label(frame3, text="Choose a category to search:", bg="gray")
+cl.place(relx=0.01, rely=0.14, anchor=tk.NW)
+
+cats = dbc.getCategoriesList()
+cb = ttk.Combobox(frame3, values=cats)
+cb.place(relx=0.01, rely=0.17, anchor=tk.NW)
+
+sb = Button(frame3, text="Search", command=videoResults)
+sb.place(relx=0.01, rely=0.2, anchor=tk.NW)
+
+rb = Listbox(frame3, width=100, height=25)
+rb.place(relx=0.01, rely=0.23, anchor=tk.NW)
+notebook.add(frame3, text="Range Q")
 
 root.mainloop()
